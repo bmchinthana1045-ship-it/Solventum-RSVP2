6a7332d03dcd4dd3c74f733144554a5a273e67a6
bmchinthana1045-ship-it <bmchinthana1045@gmail.com>
2026-09-09 15:56:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // After deploying google-apps-script/Code.gs as a Web App, paste its /exec URL here.
-const GOOGLE_SCRIPT_URL = "https://docs.google.com/spreadsheets/d/1zFuvY4THnh0eubl_9U29STcr9UgSQpeAaVLbdbE2Hys/edit?usp=sharing";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwsp0tCz87cPhaklCo-XAF6v4Shz4q4Q9OSidNFPRiCbmlDORHtIlzhc8hJEGTZZoyw/exec";
 const BACKEND_VERSION = "solventum-rsvp-standalone-v1";
 
 const questions = [
